59735aea100225b154fd640dc781c150510ee7bd
Devansh Parapalli <parapallidev@gmail.com>
2025-03-06 22:42:59 +0530

    correct issue with tailwind version mismatch

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,23 +1,6 @@
-// import tailwindcss from "tailwindcss";
-// import autoprefixer from "autoprefixer";
-// import nesting from "@tailwindcss/nesting";
-
-// export default {
-//   plugins: [nesting, tailwindcss, autoprefixer],
-// };
-
-//2nd
-// export default {
-//   plugins: {
-//     '@tailwindcss/postcss': {},
-//     autoprefixer: {},
-//   }
-// };
-
 export default {
   plugins: {
-    "postcss-nesting": {},
-    "@tailwindcss/postcss": {},
+    tailwindcss: {},
     autoprefixer: {},
   },
 };
