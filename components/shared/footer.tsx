5ef8b97f7d167989b1717006679b0f57c0931eac
Sachinp69 <sachinp9080@gmail.com>
2025-03-18 14:02:01 +0530

    connected database to clerk auth but a small problem is still there

--- components/shared/footer.tsx
+++ components/shared/footer.tsx
@@ -1,8 +1,24 @@
-import React from 'react'
+
+import Image from "next/image"
+import Link from "next/link"
 
 const Footer = () => {
   return (
-    <div>footer</div>
+    <footer className="border-t">
+      <div className="max-w-7xl md:px-10 xl:px-0 w-full wrapper flex-center flex-between flex flex-col gap-4 p-5 text-center sm:flex-row">
+        <Link href='/'>
+          <Image 
+            style={{ marginLeft: '40px' }}
+            src="/assets/images/logo.svg"
+            alt="logo"
+            width={40}
+            height={40}
+          />
+        </Link>
+
+        <p>2025 VibeQuest. No Rights reserved.</p>
+      </div>
+    </footer>
   )
 }
 
--- components/shared/header.tsx
+++ components/shared/header.tsx
@@ -1,18 +1,31 @@
-import Head from 'next/head'
+
 import React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
-import { SignedOut } from '@clerk/nextjs'
+import { SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
 import { Button } from '../ui/button'
+import NavItems from './NavItems'
+import MobileNav from './MobileNav'
 
 const Header = () => {
   return (
     <header className="w-full b-0" >
-      <div className='wrapper ml-10 flex items-center justify-between '>
-        <Link href="/" className="w-60">
+      <div className='max-w-7xl p-5 md:px-10 xl:px-0 w-full wrapper flex items-center justify-between '>
+        <Link href="/" className="w-60 px-10">
             <Image src="/assets/images/logo.jpg" alt="logo" width={60} height={60} />
         </Link>
+
+        <SignedIn>
+          <nav className="md:flex-between hidden w-full max-w-xs">
+            <NavItems />
+          </nav>
+        </SignedIn>
+
         <div className='flex w-32 justify-end gap-3'>
+          <SignedIn>
+            <MobileNav />
+            <UserButton />
+          </SignedIn>
             <SignedOut>
               <Button asChild className='rounded-full' size="lg">
                 <Link href='/sign-in'>
